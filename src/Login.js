6f7e6bbfb7c1127acad89eb4a0cd91bfd67b032d
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-08 22:19:03 -0400

    display error message on failed login

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,13 @@ import Box from "grommet/components/Box";
 import LoginForm from "grommet/components/LoginForm";
 import axios from "axios";
 
+import "./login.scss";
+
 class Login extends Component {
+  state = {
+    error: ""
+  };
+
   onSubmit = async ({ username, password, rememberMe }) => {
     try {
       const res = await axios.post(process.env.REACT_APP_API_URL + "login", {
@@ -13,18 +19,26 @@ class Login extends Component {
 
       const { access_token } = res.data;
     } catch (e) {
-      alert("invalid credentials");
+      this.setState({ error: "invalid credentials" });
     }
   };
 
   render() {
     return (
-      <Box flex full colorIndex="neutral-4-t" justify="center" align="center">
+      <Box
+        id="login-form"
+        flex
+        full
+        colorIndex="neutral-4-t"
+        justify="center"
+        align="center"
+      >
         <LoginForm
           title="My App"
           textAlign="left"
           usernameType="text"
           onSubmit={this.onSubmit}
+          errors={[this.state.error]}
         />
       </Box>
     );
