29ebccb3e90a1abe20a5bdf8165129f74daf94b4
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-08 20:41:39 -0400

    added grommet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import GrommetApp from "grommet/components/App";
+import Header from "grommet/components/Header";
+import Article from "grommet/components/Article";
+import Menu from "grommet/components/Menu";
+import Anchor from "grommet/components/Anchor";
+import Grommet from "grommet/components/Grommet";
+import Footer from "grommet/components/Footer";
+import Section from "grommet/components/Section";
+import Box from "grommet/components/Box";
+
+const appName = "MyApp";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <GrommetApp centered={false}>
+        <Box flex full>
+          <Header colorIndex="neutral-4-t" pad="small" fixed={false}>
+            <Grommet style={{ marginRight: 15, padding: "2px 8px" }}>
+              {appName}
+            </Grommet>
+            <Menu direction="row">
+              <Anchor>Header</Anchor>
+            </Menu>
+          </Header>
+          <Article flex pad="small">
+            <main>
+              <Section>My Section</Section>
+            </main>
+          </Article>
+          <Footer primary colorIndex="neutral-4-t" direction="column" pad="medium">
+            {appName}
+          </Footer>
+        </Box>
+      </GrommetApp>
     );
   }
 }
