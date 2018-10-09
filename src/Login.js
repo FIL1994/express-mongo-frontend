aace54619964d52cadefad30bd98db06decd56d1
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-08 21:59:57 -0400

    added login page

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import Box from "grommet/components/Box";
-import Heading from "grommet/components/Heading";
 import LoginForm from "grommet/components/LoginForm";
+import axios from "axios";
 
 class Login extends Component {
+  onSubmit = async ({ username, password, rememberMe }) => {
+    try {
+      const res = await axios.post(process.env.REACT_APP_API_URL + "login", {
+        username,
+        password
+      });
+
+      const { access_token } = res.data;
+    } catch (e) {
+      alert("invalid credentials");
+    }
+  };
+
   render() {
     return (
-      <Box
-        flex
-        full
-        colorIndex="neutral-4-t"
-        pad="small"
-        justify="center"
-        align="center"
-      >
-        <Heading margin="medium">My App</Heading>
-        <LoginForm textAlign="left" />
+      <Box flex full colorIndex="neutral-4-t" justify="center" align="center">
+        <LoginForm
+          title="My App"
+          textAlign="left"
+          usernameType="text"
+          onSubmit={this.onSubmit}
+        />
       </Box>
     );
   }
